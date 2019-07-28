940cbb9b561dc856c82e026fed7844754f9bc231
Casino Boyale <kayrenhi@gmail.com>
2019-07-28 10:47:19 +0100

    Added IList extensions
    Added IList extensions

---
+++ src/Casino.Common/Linq/IList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Casino.Linq
+{
+    public static partial class Extenions
+    {
+        public static T[] ToArray<T>(this IList<T> list, Func<T, bool> predicate)
+            => list.Where(predicate).ToArray();
+
+        public static List<T> ToList<T>(this IList<T> list, Func<T, bool> predicate)
+            => list.Where(predicate).ToList();
+    }
+}
